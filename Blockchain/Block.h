b8f6c4b74c50f750ad47c2c8402b9bb55d4bf439
Sturza Mihai George <sturzamihai23@gmail.com>
2020-03-24 02:36:11 +0200

    Added block and blockchain classes.

---
+++ Blockchain/Block.h
@@ -0,0 +1,11 @@
+#ifndef BLOCKCHAIN_BLOCK_H
+#define BLOCKCHAIN_BLOCK_H
+
+#endif //BLOCKCHAIN_BLOCK_H
+
+#include <cstdint>
+#include <iostream>
+
+class Block {
+    // TODO: Create block class
+};
---
+++ Blockchain/Blockchain.h
@@ -0,0 +1,12 @@
+#ifndef BLOCKCHAIN_BLOCKCHAIN_H
+#define BLOCKCHAIN_BLOCKCHAIN_H
+
+#endif //BLOCKCHAIN_BLOCKCHAIN_H
+
+#include <cstdint>
+#include <vector>
+#include "Block.h"
+
+class Blockchain {
+    // TODO: Create blockchain class
+};
