25ac7641bd5b324d0dd39f2a0e019fec3d9b0c28
Serbanescu Mihnea <serbanescu.mihnea@gmail.com>
2020-03-24 03:04:29 +0200

    Revert "Added block class"
    This reverts commit ce4b907a

--- Blockchain/Block.h
+++ Blockchain/Block.h
@@ -1,26 +1,11 @@
 #ifndef BLOCKCHAIN_BLOCK_H
 #define BLOCKCHAIN_BLOCK_H
 
+#endif //BLOCKCHAIN_BLOCK_H
+
 #include <cstdint>
 #include <iostream>
 
 class Block {
     // TODO: Create block class
-public:
-    string PrevHash;
-    Block(uint32_t IndexIn,const string &DataIn);
-    string GetHash();
-    void MineBlock(uint32_t Difficulty);
-
-private:
-    uint32_t _Index;
-    int64_t _Nonce;
-    string _Data;
-    string _Hash;
-    time_t _Time;
-
-    string _CalculateHash() const;
 };
-
-#endif //BLOCKCHAIN_BLOCK_H
-
