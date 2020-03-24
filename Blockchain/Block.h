f38eaec105e2c7b2f880d3025087a1cffd23c5ba
Serbanescu Mihnea <serbanescu.mihnea@gmail.com>
2020-03-24 03:15:08 +0200

    Added block class.

--- Blockchain/Block.h
+++ Blockchain/Block.h
@@ -1,11 +1,23 @@
 #ifndef BLOCKCHAIN_BLOCK_H
 #define BLOCKCHAIN_BLOCK_H
-
-#endif //BLOCKCHAIN_BLOCK_H
-
 #include <cstdint>
 #include <iostream>
 
 class Block {
-    // TODO: Create block class
+public:
+    std::string PrevHash;
+    Block(uint32_t IndexIn, const string &DataIn);
+    std::string GetHash();
+    void MineBlock(uint32_t Difficulty);
+
+private:
+    uint32_t _Index;
+    int64_t _Nonce;
+    std::string _Data;
+    std::string _Hash;
+    time_t _Time;
+    std::string _CalculateHash() const;
 };
+#endif //BLOCKCHAIN_BLOCK_H
+
+
